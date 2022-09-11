4107ad1798f8be481c3711a11a8f15500154c2e1
Taioh Kubota <kubotataioh@gmail.com>
2022-09-11 17:51:55 -0400

    Add more CORS rules

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+})
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(authenticateJWT);
